bc6c5d430a8a33bbb4721c6fee640e2a14e0c320
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 09:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-/*describe("Draggable and Droppable Test", () => {
-  beforeEach(() => {
-    cy.visit("your_website_url");
+  $(function() {
+    $(".draggable").draggable({
+      revert: "invalid",
+      stack: ".draggable"
+    });
+    $(".container").droppable({
+      accept: ".draggable",
+      drop: function(event, ui) {
+        var draggableId = ui.draggable.attr("id");
+        var droppableId = $(this).find(".ui-droppable-active").attr("id");
+        var draggableImage = ui.draggable.css("background-image");
+        var droppableImage = $("#" + droppableId).css("background-image");
+
+        ui.draggable.css("background-image", droppableImage);
+        $("#" + droppableId).css("background-image", draggableImage);
+      }
+    });
   });
+let currentDroppable = null;
 
-  it("Should have 6 draggable elements", () => {
-    for (let index = 1; index <= 6; index++) {
-      cy.get(`#div${index}`).should("have.length", 1);
-    }
-  });
-
-  it("Should be able to drag elements", () => {
-    for (let index = 1; index <= 6; index++) {
-      cy.get(`#div${index}`)
-        .trigger("mousedown", { which: 1, force: true })
-        .trigger("mousemove", { clientX: 500, clientY: 600, force: true })
-        .trigger("mouseup", { force: true });
-    }
-  });
-
-  it("Should swap background images on drop", () => {
-    cy.get("#div1").trigger("mousedown", { which: 1, force: true });
-    cy.get("#div2").trigger("mousemove", { clientX: 500, clientY: 600, force: true });
-    cy.get("#div2").trigger("mouseup", { force: true });
-
-    cy.get("#div1").should("have.css", "background-image", "url('https://picsum.photos/seed/picsum/200/300')");
-    cy.get("#div2").should("have.css", "background-image", "url('https://picsum.photos/id/237/200/300')");
-  });
-
-  it("Should visually indicate draggable elements", () => {
-    for (let index = 1; index <= 6; index++) {
-      cy.get(`#div${index}`).should("have.css", "cursor", "move");
-    }
-  });
-
-  it("Should accept draggable elements as drop targets", () => {
-    cy.get("#div1").trigger("mousedown", { which: 1, force: true });
-    cy.get("#div2").trigger("mousemove", { clientX: 500, clientY: 600, force: true });
-    cy.get("#div2").trigger("mouseup", { force: true });
-
-    cy.get("#div1").should("have.css", "background-image", "url('https://picsum.photos/seed/picsum/200/300')");
-    cy.get("#div2").should("have.css", "background-image", "url('https://picsum.photos/id/237/200/300')");
-  });
-
-  // Add more tests here to cover other scenarios
+$(".draggable").draggable({
+  revert: "invalid",
+  stack: ".draggable"
 });
-*/
+
+$(".draggable").droppable({
+  over: function(event, ui) {
+    currentDroppable = $(this);
+  },
+  out: function(event, ui) {
+    currentDroppable = null;
+  },
+  drop: function(event, ui) {
+    if (currentDroppable) {
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = currentDroppable.attr("id");
+      var draggableImage = ui.draggable.css("background-image");
+      var droppableImage = currentDroppable.css("background-image");
+
+      ui.draggable.css("background-image", droppableImage);
+      currentDroppable.css("background-image", draggableImage);
+    }
+  }
+});
+
