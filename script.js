cbac6325b73e4dc6f400803460cc360f4ec6bb09
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 09:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,71 +1,43 @@
-$(function() {
-  $(".draggable").draggable({
-    revert: "invalid",
-    stack: ".draggable"
+describe("Draggable and Droppable Test", () => {
+  beforeEach(() => {
+    cy.visit("your_website_url");
   });
 
-  $(".container").droppable({
-    accept: ".draggable",
-    drop: function(event, ui) {
-      var draggableId = ui.draggable.attr("id");
-      var droppableId = $(this).attr("id"); // Simplified to use the droppable container's ID directly
-      var draggableImage = ui.draggable.css("background-image");
-      var droppableImage = $(this).css("background-image"); // Simplified to use $(this)
-
-      console.log("Droppable element id: " + droppableId);
-      console.log("Draggable element id: " + draggableId);
-      console.log("Draggable element background image before swap: " + draggableImage);
-      console.log("Droppable element background image before swap: " + droppableImage);
-
-      ui.draggable.css("background-image", droppableImage);
-      $(this).css("background-image", draggableImage); // Simplified to use $(this)
-
-      console.log("Draggable element background image after swap: " + ui.draggable.css("background-image"));
-      console.log("Droppable element background image after swap: " + $(this).css("background-image")); // Simplified to use $(this)
+  it("Should have 6 draggable elements", () => {
+    for (let index = 1; index <= 6; index++) {
+      cy.get(`#drag${index}`).should("have.length", 1);
     }
   });
 
-  let currentDroppable = null;
-
-  $(".draggable").draggable({
-    revert: "invalid",
-    stack: ".draggable",
-    over: function(event, ui) {
-      currentDroppable = $(this);
-      console.log("Draggable element is over droppable element");
-    },
-    out: function(event, ui) {
-      currentDroppable = null;
-      console.log("Draggable element is out of droppable element");
+  it("Should be able to drag elements", () => {
+    for (let index = 1; index <= 6; index++) {
+      cy.get(`#drag${index}`).trigger("mousedown", { which: 1 });
+      cy.get(`#drag${index}`).trigger("mousemove", { clientX: 500, clientY: 600 });
+      cy.get(`#drag${index}`).trigger("mouseup");
     }
   });
 
-  $(".draggable").droppable({
-    drop: function(event, ui) {
-      if (currentDroppable) {
-        var draggableId = ui.draggable.attr("id");
-        var droppableId = currentDroppable.attr("id");
-        var draggableImage = ui.draggable.css("background-image");
-        var droppableImage = currentDroppable.css("background-image");
+  it("Should swap background images on drop", () => {
+    cy.get("#drag1").trigger("mousedown", { which: 1 });
+    cy.get("#drag2").trigger("mousemove", { clientX: 500, clientY: 600 });
+    cy.get("#drag2").trigger("mouseup");
 
-        console.log("Droppable element id: " + droppableId);
-        console.log("Draggable element id: " + draggableId);
-        console.log("Draggable element background image before swap: " + draggableImage);
-        console.log("Droppable element background image before swap: " + droppableImage);
+    cy.get("#drag1").should("have.css", "background-image", "url('https://picsum.photos/seed/picsum/200/300')");
+    cy.get("#drag2").should("have.css", "background-image", "url('https://picsum.photos/id/237/200/300')");
+  });
 
-        ui.draggable.css("background-image", droppableImage);
-        currentDroppable.css("background-image", draggableImage);
-
-        console.log("Draggable element background image after swap: " + ui.draggable.css("background-image"));
-        console.log("Droppable element background image after swap: " + currentDroppable.css("background-image"));
-      }
+  it("Should visually indicate draggable elements", () => {
+    for (let index = 1; index <= 6; index++) {
+      cy.get(`#drag${index}`).should("have.css", "cursor", "move");
     }
+  });
+
+  it("Should accept draggable elements as drop targets", () => {
+    cy.get("#drag1").trigger("mousedown", { which: 1 });
+    cy.get("#drag2").trigger("mousemove", { clientX: 500, clientY: 600 });
+    cy.get("#drag2").trigger("mouseup");
+
+    cy.get("#drag1").should("have.css", "background-image", "url('https://picsum.photos/seed/picsum/200/300')");
+    cy.get("#drag2").should("have.css", "background-image", "url('https://picsum.photos/id/237/200/300')");
   });
 });
-
-it('should verify existence of draggable elements', () => {
-  for (let index = 1; index <= 6; index++) {
-    cy.get(`#drag${index}`).should("have.length", 1);
-  }
-});
-
