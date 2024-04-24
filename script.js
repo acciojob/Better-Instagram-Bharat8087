b9e7b521614fd663dc54f958436544537165a750
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 10:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ describe("Draggable and Droppable Test", () => {
   });
 
   it("Should be able to drag elements", () => {
-    for (let index = 1; index <= 6; index++) {
-      cy.get(`#drag${index}`).trigger("mousedown", { which: 1 });
-      cy.get(`#drag${index}`).trigger("mousemove", { clientX: 500, clientY: 600 });
-      cy.get(`#drag${index}`).trigger("mouseup");
-    }
+    const draggable = Cypress.$("#drag3")[0]; // Pick up this
+    const droppable = Cypress.$("#drag6")[0]; // Drop over this
+    const coords = droppable.getBoundingClientRect();
+
+    draggable.dispatchEvent(new MouseEvent("mousedown"));
+    draggable.dispatchEvent(new MouseEvent("mousemove", { clientX: 10, clientY: 0 }));
+    draggable.dispatchEvent(new MouseEvent("mousemove", { clientX: coords.x + 10, clientY: coords.y + 10 }));
+    draggable.dispatchEvent(new MouseEvent("mouseup"));
+
+    cy.get("#div6").within(() => {
+      cy.get("img").should("have.length", 1);
+    });
   });
 
   it("Should swap background images on drop", () => {
