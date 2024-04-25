47941be3e3dbe699ad413b704866db15b0f64956
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-25 09:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 () => {
     const draggable = Cypress.$("#drag3")[0]; // Pick up this
     const droppable = Cypress.$("#drag6")[0]; // Drop over this
-    const draggableRect = draggable.getBoundingClientRect();
-    const droppableRect = droppable.getBoundingClientRect();
 
-    draggable.dispatchEvent(new MouseEvent("pointerdown", {
-        clientX: draggableRect.x + draggableRect.width / 2,
-        clientY: draggableRect.y + draggableRect.height / 2,
-        force: true
-    }));
+    if (!draggable || !droppable) {
+        throw new Error("Could not find draggable or droppable elements.");
+    }
 
-    draggable.dispatchEvent(new MouseEvent("pointermove", {
-        clientX: droppableRect.x + droppableRect.width / 2,
-        clientY: droppableRect.y + droppableRect.height / 2,
-        force: true
-    }));
+    const coords = droppable.getBoundingClientRect();
 
-    draggable.dispatchEvent(new MouseEvent("pointerup", {
-        force: true
-    }));
+    if (!coords) {
+        throw new Error("Could not get bounding client rect of droppable element.");
+    }
+
+    draggable.dispatchEvent(new MouseEvent("mousedown"));
+    draggable.dispatchEvent(new MouseEvent("mousemove", { clientX: 10, clientY: 0 }));
+    draggable.dispatchEvent(new MouseEvent("mousemove", { clientX: coords.x + 10, clientY: coords.y + 10 }));
+    draggable.dispatchEvent(new MouseEvent("mouseup"));
 
     cy.get("#div6").within(() => {
         cy.get("img").should("have.length", 1);
