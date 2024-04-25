79c3dd0f476c3c72368303e49ba9afdb98ad7702
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-25 08:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,6 +21,42 @@ $(document).ready(function() {
             } else {
                 droppable.before(draggable);
             }
+
+            draggable.draggable({
+                revert: "invalid",
+                helper: "clone"
+            });
+
+            draggable.droppable({
+                accept: ".draggable",
+                drop: function(event, ui) {
+                    var dragged = ui.draggable;
+                    var droppedOn = $(this);
+
+                    var parentContainer = droppedOn.parent();
+
+                    dragged.detach();
+
+                    dragged.appendTo(parentContainer);
+
+                    if (dragged.index() < droppedOn.index()) {
+                        droppedOn.after(dragged);
+                    } else {
+                        droppedOn.before(dragged);
+                    }
+
+                    dragged.draggable({
+                        revert: "invalid",
+                        helper: "clone"
+                    });
+
+                    dragged.droppable({
+                        accept: ".draggable",
+                        drop: function(event, ui) {
+                        }
+                    });
+                }
+            });
         }
     });
 });
