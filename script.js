399cc234a38c1b5fb9792a5cf35343225f3b3f88
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-25 08:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function handleDrop(event, ui) {
+    var draggable = ui.draggable;
+    var droppable = $(this);
+
+    var parentContainer = droppable.parent();
+
+    draggable.detach();
+    draggable.appendTo(parentContainer);
+
+    if (draggable.index() < droppable.index()) {
+        droppable.after(draggable);
+    } else {
+        droppable.before(draggable);
+    }
+
+    draggable.draggable({
+        revert: "invalid",
+        helper: "clone"
+    });
+
+    draggable.droppable({
+        accept: ".draggable",
+        drop: handleDrop
+    });
+}
+
 $(document).ready(function() {
     $(".draggable").draggable({
         revert: "invalid",
@@ -6,57 +32,6 @@ $(document).ready(function() {
 
     $(".draggable").droppable({
         accept: ".draggable",
-        drop: function(event, ui) {
-            var draggable = ui.draggable;
-            var droppable = $(this);
-
-            var parentContainer = droppable.parent();
-
-            draggable.detach();
-
-            draggable.appendTo(parentContainer);
-
-            if (draggable.index() < droppable.index()) {
-                droppable.after(draggable);
-            } else {
-                droppable.before(draggable);
-            }
-
-            draggable.draggable({
-                revert: "invalid",
-                helper: "clone"
-            });
-
-            draggable.droppable({
-                accept: ".draggable",
-                drop: function(event, ui) {
-                    var dragged = ui.draggable;
-                    var droppedOn = $(this);
-
-                    var parentContainer = droppedOn.parent();
-
-                    dragged.detach();
-
-                    dragged.appendTo(parentContainer);
-
-                    if (dragged.index() < droppedOn.index()) {
-                        droppedOn.after(dragged);
-                    } else {
-                        droppedOn.before(dragged);
-                    }
-
-                    dragged.draggable({
-                        revert: "invalid",
-                        helper: "clone"
-                    });
-
-                    dragged.droppable({
-                        accept: ".draggable",
-                        drop: function(event, ui) {
-                        }
-                    });
-                }
-            });
-        }
+        drop: handleDrop
     });
 });
