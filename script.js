67c91573008cc329b2a477fcb6ec6e6cccb21936
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-25 09:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-function handleDrop(event, ui) {
-    var draggable = ui.draggable;
-    var droppable = $(this);
-
-    var parentContainer = droppable.parent();
-
-    draggable.detach();
-    draggable.appendTo(parentContainer);
-
-    if (draggable.index() < droppable.index()) {
-        droppable.after(draggable);
-    } else {
-        droppable.before(draggable);
-    }
-
-    draggable.draggable({
-        revert: "invalid",
-        helper: "clone"
-    });
-
-    draggable.droppable({
-        accept: ".draggable",
-        drop: handleDrop
-    });
-}
-
 $(document).ready(function() {
     $(".draggable").draggable({
         revert: "invalid",
-        helper: "clone"
+        helper: "clone",
+        zIndex: 100
     });
 
     $(".draggable").droppable({
         accept: ".draggable",
-        drop: handleDrop
+        drop: function(event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
+
+            var tempImage = draggable.css("background-image");
+            draggable.css("background-image", droppable.css("background-image"));
+            droppable.css("background-image", tempImage);
+        }
     });
 });
